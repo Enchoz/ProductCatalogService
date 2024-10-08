51932d686834db9d59d0a1747bb34904d96e055b
EneOzioma <129067524+EneOzioma@users.noreply.github.com>
2024-10-08 23:12:27 +0100

    Add HealthCheck endpoint

---
+++ src/ProductService/API/Controllers/HealthCheckController .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductService.Domain.Entities;
+using ProductService.Services;
+using ProductService.Services.Interfaces;
+
+namespace ProductService.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "Healthy", timestamp = DateTime.UtcNow });
+        }
+    }
+}
